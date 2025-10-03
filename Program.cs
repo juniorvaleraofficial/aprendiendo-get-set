11a2fdd833ff0ecea90d9ae281ff2cb978cf5c0f
Junior Valera <detoprox@gmail.com>
2025-10-03 19:15:52 -0400

    // c.Saldo = 999m; // No debe compilar

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     var ObjProducto = new Producto();
     var e = new Estudiante();
     var r = new Reloj();
+    var c = new Cuenta();
     ObjetoPersona.Nombre = "Junior";
     ObjApellido.Apellido = " Valera";
     ObjProducto.Nombre = "Laptop Gamer: ";
     e.Edad = 30;
+    c.Depositar(100m);
     Console.WriteLine(ObjetoPersona.Nombre + ObjApellido.Apellido);
     Console.WriteLine(ObjProducto.Nombre);
     Console.WriteLine($"La Edad es: {e.Edad}");
     Console.WriteLine(r.Zona);
+    Console.WriteLine(c.Depositar);
