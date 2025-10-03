2f28e626adf6bfb1b6c431dd8ccdeab42e30a082
Junior Valera <detoprox@gmail.com>
2025-10-03 18:28:03 -0400

    Objetivo: Usar auto-properties para simplificar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
     var ObjetoPersona = new Persona();
     var ObjApellido = new Persona();
+    var ObjProducto = new Producto();
     ObjetoPersona.Nombre = "Junior";
     ObjApellido.Apellido = " Valera";
+    ObjProducto.Nombre = "Laptop Gamer: ";
     Console.WriteLine(ObjetoPersona.Nombre + ObjApellido.Apellido);
+    Console.WriteLine(ObjProducto.Nombre);
