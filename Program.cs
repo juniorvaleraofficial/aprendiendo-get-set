efdb79b736968d9715feb58748de300d3390b1b9
Junior Valera <detoprox@gmail.com>
2025-10-03 16:48:38 -0400

    Objetivo: Encapsular un campo con get/set.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+    var ObjetoPersona = new Persona();
+    var ObjApellido = new Persona();
+    ObjetoPersona.Nombre = "Junior";
+    ObjApellido.Apellido = " Valera";
+    Console.WriteLine(ObjetoPersona.Nombre + ObjApellido.Apellido);
