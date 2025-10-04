e0d2028d31a310fbc1c9e6b479328e1cc3666269
Junior Valera <detoprox@gmail.com>
2025-10-04 17:03:44 -0400

    Objetivo: Mostrar cómo una propiedad refleja otra.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var a = new Usuario();
-a.Email = "detoprox@gmai.com";
-Console.WriteLine($"Email valido {a.Email}");
-a.Email = "Invalido";
-Console.WriteLine($"Email invalido {a.Email}");
+var t = new Termometro();
+        t.Celsius = 1;
+        t.Fahrenheit = 212;
+        Console.WriteLine(t.Fahrenheit); // 32
+        Console.WriteLine(t.Celsius);    // 100
